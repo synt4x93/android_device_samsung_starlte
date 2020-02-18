28ac542a264e5aed54a785555a5aa51017caacca
synt4x93 <synt4x.93@gmail.com>
2020-02-18 04:12:52 +0000

    starlte: update repo

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -3,6 +3,11 @@
 
 #define BTM_DEF_LOCAL_NAME   "Galaxy S9"
 
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
+
 #define BLE_VND_INCLUDED TRUE
+
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #endif
--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -1,12 +1,26 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-$(call inherit-product, device/samsung/starlte/full_starlte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, vendor/samsung/universal9810-common/starlte/universal9810-common-vendor.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/starlte/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := starlte
 PRODUCT_NAME := lineage_starlte
+PRODUCT_MODEL := SM-G960F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 BUILD_FINGERPRINT := samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXU7DTAA:user/release-keys
 
